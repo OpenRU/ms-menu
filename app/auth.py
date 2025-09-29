0c6ae2c72595cada1d637fe0cf90ffdaaa37f79e
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-29 20:48:07 -0300

    refactor: envia o token no header para validação

--- app/auth.py
+++ app/auth.py
@@ -21,7 +21,7 @@ def is_authenticated():
 
     try:
         endpoint = f"{AUTH_URI}/auth/validate-token/"
-        response = requests.get(endpoint, data={'token': token})
+        response = requests.get(endpoint, headers={'token': token})
 
         if response.status_code == 200:
             return True
